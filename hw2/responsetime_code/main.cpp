bbdc9a9d9e22075cd53f63a8e536ff9b9ab36fdf
aarontravass <aaron.travass@hotmail.com>
2022-03-03 01:44:01 -0500

    added

--- hw2/responsetime_code/main.cpp
+++ hw2/responsetime_code/main.cpp
@@ -2,31 +2,37 @@
 
 using namespace std;
 
+void printer(vector<int> a){
+    for(int itr=0;itr<a.size();itr++){
+        cout<<a[itr]<<" ";
+    }
+    cout<<endl;
+}
+
 int main()
 {
     vector<int> c = {3,3,5};
     vector<int> t = {7,12,20};
-    vector<int> w(0,c.size(),0), r(0,c.size(),0);
-
-    for(int i=0;i<c.size();i++){
+    vector<int> w(c.size(),0), r(c.size(),0);
+    w[0] =c[0];
+    for(int i=1;i<c.size();i++){
         //w[i]+=c[i];
-        int current[2] = {0,c[i]};
+        int current[2] = {c[i],c[i]};
         while(1){
-            int last = current[1];
-            for(int j=i;j>=0;j--){
-                current[1]+=c[i]*ceil(last/t[i]);
+            current[1] = c[i];
+            for(int j=0;j<i;j++){
+                current[1]+=(c[i-1]*ceil((float)current[0]/(float)t[j]));
             }
             if(current[0]==current[1]){
-                r[i]=current[0];
+                w[i]=current[0];
                 break;
             }
-            else if(current[1]>t[i]){
-                r[i]=-1;
+            if(current[1]>t[i]){
                 break;
             }
             current[0]=current[1];
-
         }
     }
+    printer(w);
     return 0;
 }
